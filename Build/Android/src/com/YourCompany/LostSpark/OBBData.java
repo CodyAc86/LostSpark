1d4fbb9df1027fc569a25e74036c701786969977
Aurel Codarcea <aurel.codarcea86@gmail.com>
2025-09-17 14:59:48 +0300

    Big Updates too many to count

--- Build/Android/src/com/YourCompany/LostSpark/OBBData.java
+++ Build/Android/src/com/YourCompany/LostSpark/OBBData.java
@@ -19,7 +19,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-340136352L // the length of the file in bytes
+173667624L // the length of the file in bytes
 )
 };
 };
