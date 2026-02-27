cf9f6b24ba4df8f2866f3fbf6ddbd97b021b35c9
Aurel Codarcea <aurel.codarcea86@gmail.com>
2026-02-27 16:21:49 +0200

    Reverted

--- Build/Android/src/com/SurvivingPixels/Sparks/OBBData.java
+++ Build/Android/src/com/SurvivingPixels/Sparks/OBBData.java
@@ -19,7 +19,11 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
+<<<<<<< Updated upstream
 159101271L // the length of the file in bytes
+=======
+159015508L // the length of the file in bytes
+>>>>>>> Stashed changes
 )
 };
 };
