c46f9b45e81daff92b2d2e29bf7cc5e8079eee71
Aurel Codarcea <aurel.codarcea86@gmail.com>
2026-02-27 21:00:13 +0200

    working now

--- Build/Android/src/com/SurvivingPixels/Sparks/OBBData.java
+++ Build/Android/src/com/SurvivingPixels/Sparks/OBBData.java
@@ -2,29 +2,25 @@ package com.SurvivingPixels.Sparks;
 
 public class OBBData
 {
-public static final String AppType = "";
+    public static final String AppType = "";
 
-public static class XAPKFile {
-public final boolean mIsMain;
-public final String mFileVersion;
-public final long mFileSize;
-XAPKFile(boolean isMain, String fileVersion, long fileSize) {
-mIsMain = isMain;
-mFileVersion = fileVersion;
-mFileSize = fileSize;
+    public static class XAPKFile {
+        public final boolean mIsMain;
+        public final String mFileVersion;
+        public final long mFileSize;
+
+        XAPKFile(boolean isMain, String fileVersion, long fileSize) {
+            mIsMain = isMain;
+            mFileVersion = fileVersion;
+            mFileSize = fileSize;
+        }
+    }
+
+    public static final XAPKFile[] xAPKS = {
+        new XAPKFile(
+            true,          // true signifies a main file
+            "1",           // the version of the APK that the file was uploaded against
+            159101271L     // the length of the file in bytes (Updated upstream)
+        )
+    };
 }
-}
-
-public static final XAPKFile[] xAPKS = {
-new XAPKFile(
-true, // true signifies a main file
-"1", // the version of the APK that the file was uploaded against
-<<<<<<< Updated upstream
-159101271L // the length of the file in bytes
-=======
-159015508L // the length of the file in bytes
->>>>>>> Stashed changes
-)
-};
-};
-
